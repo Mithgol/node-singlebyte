9ed1805481c2864c024e7236e3a4f544ece5dd08
Mithgol the Webmaster <getgit@mithgol.ru>
2013-09-01 11:27:34 +0400

    added .isEncoding(), started .bufToString

--- singlebyte.js
+++ singlebyte.js
@@ -1,7 +1,33 @@
+//var _ = require('underscore');
+
 var singlebyte = function(){
    if(!( this instanceof singlebyte )){
       return new singlebyte();
    }
-}
+
+   this.encodings = [];
+};
+
+singlebyte.prototype.isEncoding = function(encodingName){
+   if( Buffer.isEncoding(encodingName) ) return true;
+   for( var i = 0; i < this.encodings.length; i++ ){
+      if( this.encodings[i].name === encodingName ) return true;
+   }
+   return false;
+};
+
+singlebyte.prototype.bufToString = function(buf, encoding){
+   if(!( Buffer.isBuffer(buf) )){
+      throw new Error(this.errors.NOT_A_BUFFER);
+   }
+   if(!( this.isEncoding(encoding) )){
+      throw new Error(this.errors.INVALID_ENCODING);
+   }
+};
+
+singlebyte.prototype.errors = {
+   NOT_A_BUFFER : 'The given source is not a buffer!',
+   INVALID_ENCODING: 'The given encoding is not supported!'
+};
 
 module.exports = singlebyte;
